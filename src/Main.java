49360d995d5769c2bdf72cff09baed3b94ca2e76
Nika <ver.semionova2015@mail.ru>
2023-10-23 16:25:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray(30000000);
+
+
+        int[] arr1 = arr;
+        long startTime = System.currentTimeMillis();
+        Arrays.sort(arr); // Обычная сортировка в один поток
+        long endTime = System.currentTimeMillis();
+        System.out.println("Время выполнения обычной сортировки: " + (endTime - startTime) + " миллисекунд");
+
+        // Вывод первых 50 чисел из обычно отсортированного массива
+        System.out.println("Первые 50 чисел из обычно отсортированного массива:");
+        for (int i = 0; i < 50; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        ForkJoinPool pool = new ForkJoinPool(4);
+        startTime = System.currentTimeMillis();
+        int[] sortedArray = pool.invoke(new ParallelSortTask(arr1)); // Параллельная сортировка
+        endTime = System.currentTimeMillis();
+        System.out.println("Время выполнения параллельной сортировки: " + (endTime - startTime) + " миллисекунд");
+
+        // Вывод первых 50 чисел из параллельно отсортированного массива
+        System.out.println("Первые 50 чисел из параллельно отсортированного массива:");
+        for (int i = 0; i < 50; i++) {
+            System.out.print(sortedArray[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] generateRandomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * size * 10); // Генерируем случайные числа
+        }
+        return arr;
+    }
+
+    static class ParallelSortTask extends RecursiveTask<int[]> {
+        private int[] array;
+
+        ParallelSortTask(int[] array) {
+            this.array = array;
+        }
+
+        @Override
+        protected int[] compute() {
+            if (array.length <= 1000) {
+                Arrays.sort(array);
+                return array;
+            } else {
+                int mid = array.length / 2;
+                int[] left = Arrays.copyOfRange(array, 0, mid);
+                int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+                ParallelSortTask leftTask = new ParallelSortTask(left);
+                ParallelSortTask rightTask = new ParallelSortTask(right);
+
+                leftTask.fork();
+                rightTask.fork();
+
+                int[] leftResult = leftTask.join();
+                int[] rightResult = rightTask.join();
+
+                return merge(leftResult, rightResult);
+            }
+        }
+
+        private int[] merge(int[] left, int[] right) {
+            int[] result = new int[left.length + right.length];
+            int i = 0, j = 0, k = 0;
+            while (i < left.length && j < right.length) {
+                if (left[i] < right[j]) {
+                    result[k++] = left[i++];
+                } else {
+                    result[k++] = right[j++];
+                }
+            }
+            while (i < left.length) {
+                result[k++] = left[i++];
+            }
+            while (j < right.length) {
+                result[k++] = right[j++];
+            }
+            return result;
+        }
+    }
+}
